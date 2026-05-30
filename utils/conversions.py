1aab6964a13ecf2675d49c3c2628a311c84411d7
thanhxuan2k5 <xuanng205@gmail.com>
2026-05-30 23:29:27 +0700

    push4

--- utils/conversions.py
+++ utils/conversions.py
@@ -1,5 +1,6 @@
-def convert_pixel_distance_to_meters(pixel_distance, reference_height_in_meters, reference_height_in_pixels):
-    return (pixel_distance * reference_height_in_meters) / reference_height_in_pixels
 
-def convert_meters_to_pixel_distance(meters, reference_height_in_meters, reference_height_in_pixels):
-    return (meters * reference_height_in_pixels) /reference_height_in_meters
+def convert_pixel_distance_to_meters(pixel_distance, refrence_height_in_meters, refrence_height_in_pixels):
+    return (pixel_distance * refrence_height_in_meters) / refrence_height_in_pixels
+
+def convert_meters_to_pixel_distance(meters, refrence_height_in_meters, refrence_height_in_pixels):
+    return (meters * refrence_height_in_pixels) / refrence_height_in_meters
